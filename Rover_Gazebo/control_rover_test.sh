971b03d9e79d1e87ef669d3f6a4835bb5e0d874f
leonello20 <113144307+leonello20@users.noreply.github.com>
2025-03-06 14:19:06 -0500

    Rover Gazebo

---
+++ Rover_Gazebo/control_rover_test.sh
@@ -0,0 +1,4 @@
+gz topic -t "/right_front_wheel_joint_vel" -m gz.msgs.Double -p "data: 2"
+gz topic -t "/left_front_wheel_joint_vel" -m gz.msgs.Double -p "data: 2"
+#gz topic -t "/right_back_wheel_joint_vel" -m gz.msgs.Double -p "data: 2"
+#gz topic -t "/left_back_wheel_joint_vel" -m gz.msgs.Double -p "data: 2"
