7690b1b9f7fb4630050d0be60c87d71730450d6a
leonello20 <113144307+leonello20@users.noreply.github.com>
2025-04-08 22:24:46 -0400

    Update control_rover_test.sh

--- Rover_Gazebo/control_rover_test.sh
+++ Rover_Gazebo/control_rover_test.sh
@@ -1,4 +1,25 @@
-gz topic -t "/right_front_wheel_joint_vel" -m gz.msgs.Double -p "data: 2"
-gz topic -t "/left_front_wheel_joint_vel" -m gz.msgs.Double -p "data: 2"
-#gz topic -t "/right_back_wheel_joint_vel" -m gz.msgs.Double -p "data: 2"
-#gz topic -t "/left_back_wheel_joint_vel" -m gz.msgs.Double -p "data: 2"
+gz topic -t "/clearance_arm_joint_pos" -m gz.msgs.Double -p "data: 0.6"
+sleep 2
+gz topic -t "/shoulder_mount_joint_pos" -m gz.msgs.Double -p "data: 1"
+sleep 6
+gz topic -t "/right_front_wheel_joint_pos" -m gz.msgs.Double -p "data: 250"
+gz topic -t "/left_front_wheel_joint_pos" -m gz.msgs.Double -p "data: -250"
+gz topic -t "/right_back_wheel_joint_pos" -m gz.msgs.Double -p "data: 250"
+gz topic -t "/left_back_wheel_joint_pos" -m gz.msgs.Double -p "data: -250"
+sleep 12
+gz topic -t "/joint1_pos" -m gz.msgs.Double -p "data: 4"
+sleep 4
+#gz topic -t "/joint2_pos" -m gz.msgs.Double -p "data: 2"
+sleep 4
+gz topic -t "/joint3_pos" -m gz.msgs.Double -p "data: 1"
+sleep 4
+gz topic -t "/joint4_pos" -m gz.msgs.Double -p "data: 2"
+sleep 4
+#gz topic -t "/joint5_pos" -m gz.msgs.Double -p "data: 3"
+#sleep 4
+#gz topic -t "/joint6_pos" -m gz.msgs.Double -p "data: 2"
+sleep 4
+#gz topic -t "/joint1_pos" -m gz.msgs.Double -p "data: 2"
+sleep 4
+gz topic -t "/gripper_right_joint_pos" -m gz.msgs.Double -p "data: 2"
+gz topic -t "/gripper_left_joint_pos" -m gz.msgs.Double -p "data: -2"
